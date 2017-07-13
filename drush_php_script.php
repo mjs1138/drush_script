655515c98ee8306c2e03c21465c277b20e92d745
Mike <mike@scllc.us>
2017-07-13 11:09:45 -0400

    Added Confirm Interaction.

--- drush_php_script.php
+++ drush_php_script.php
@@ -1,20 +1,20 @@
 <?php
 // Select local machine or
 $local = $argv[1];
-if((strcmp($local, "local") && (strcmp($local, "remote")))){
+if ((strcmp($local, "local") && (strcmp($local, "remote")))) {
     echo("You must choose 'local' or 'remote'\n");
     exit;
 }
 
 // ///////////////// SET PARMS /////////////////////////////////////////////////
-$target_path = null;
-$domain = null;
-$subdomain = null; // starts as null
+$target_path = NULL;
+$domain = NULL;
+$subdomain = NULL; // starts as null
 $target_sites = 'drush_target_sites.php'; // some version of the 'sites.php' file
-$root = null;
+$root = NULL;
 
 // /////////////////// SET UP & TEST ENVIRONMENT //////////////////////////////////////
-if(!strcmp($local, "local")){
+if (!strcmp($local, "local")) {
     $target_path = '/Applications/MAMP/drush_script/';
     $root = '/Applications/MAMP/htdocs_756_multisite'; // Location of Drupal index file
     $domain = 'local.vermont.gov'; // The domain
@@ -27,69 +27,92 @@ if(!strcmp($local, "local")){
 $filePathName = "$target_path/$target_sites";
 if (file_exists($filePathName)) {
     echo "The file $filePathName exists\n";
-    $file = fopen($filePathName,"r");
+    ProcessFiles($filePathName, $domain, $root, FALSE);
+
+    $message = "Are you sure you want to do this [y/N]";
+    print $message;
+    flush();
+    ob_flush();
+    $confirmation = trim(fgets(STDIN));
+    if ($confirmation !== 'y') {
+        // The user did not say 'y'.
+        exit (0);
+    } else {
+        ProcessFiles($filePathName, $domain, $root, TRUE);
+    }
 } else {
     echo "The target-sites file: $filePathName does not exist\n";
     exit;
 }
 
 // /////////////////////////////////////////////////////////////////////////////
-while(! feof($file))
+
+function ProcessFiles($filePathName, $domain, $root, $execute)
 {
-    $fileLine = trim(fgets($file)); // read line from file
+    $tgtChars = ["'", ";"];
+    $file = fopen($filePathName, "r");
 
-    if($fileLine == ''){ //skip lines that are blank
-        continue;
+    if (!$execute) {
+        echo "These Sites Will Be Process:\n";
     }
+    while (!feof($file)) {
+        $fileLine = trim(fgets($file)); // read line from file
 
-    $siteLine = substr_compare($fileLine, '$sites[\'', 0, 8); // Test if line starts for beginning with $sites[
+        if ($fileLine == '') { //skip lines that are blank
+            continue;
+        }
 
-    if(!$siteLine) {
-        $pieces = explode('=', $fileLine); // split the line @ '='
-        $tgtChars = array("'", ";");
+        $siteLine = substr_compare($fileLine, '$sites[\'', 0, 8); // Test if line starts for beginning with $sites[
 
-        $subdomain = trim(str_replace($tgtChars, "", $pieces[1])); // delete single quotes and semicolons
-        echo $subdomain;
-        echo "\r\n";
+        if (!$siteLine) {
+            $pieces = explode('=', $fileLine); // split the line @ '='
 
-        $operation = " pm-enable features ";
-        $command = "drush --root=$root --uri=$subdomain.$domain/ $operation -y";
-        echo "COMMAND: " . $command . "\r\n";
-        $output = shell_exec($command);
+            if ($execute) {
+                $subdomain = trim(str_replace($tgtChars, "", $pieces[1])); // delete single quotes and semicolons
+                echo $subdomain;
+                echo "\r\n";
 
-        $operation = " pm-enable vic_filtered_html_table_text_format ";
-        $command = "drush --root=$root --uri=$subdomain.$domain/ $operation -y";
-        echo "COMMAND: " . $command . "\r\n";
-        $output = shell_exec($command);
+                $operation = " pm-enable features ";
+                $command = "drush --root=$root --uri=$subdomain.$domain/ $operation -y";
+                echo "COMMAND: " . $command . "\r\n";
+                $output = shell_exec($command);
 
-        $operation = " role-add-perm 'Site Administrator' 'use text format filtered_html_tables' ";
-        $command = "drush --root=$root --uri=$subdomain.$domain/ $operation -y";
-        echo "COMMAND: " . $command . "\r\n";
-        $output = shell_exec($command);
+                $operation = " pm-enable vic_filtered_html_table_text_format ";
+                $command = "drush --root=$root --uri=$subdomain.$domain/ $operation -y";
+                echo "COMMAND: " . $command . "\r\n";
+                $output = shell_exec($command);
 
-        $operation = " role-add-perm 'Enterprise Administrator' 'use text format filtered_html_tables' ";
-        $command = "drush --root=$root --uri=$subdomain.$domain/ $operation -y";
-        echo "COMMAND: " . $command . "\r\n";
-        $output = shell_exec($command);
+                $operation = " role-add-perm 'Site Administrator' 'use text format filtered_html_tables' ";
+                $command = "drush --root=$root --uri=$subdomain.$domain/ $operation -y";
+                echo "COMMAND: " . $command . "\r\n";
+                $output = shell_exec($command);
 
-        $operation = " role-remove-perm 'Site Administrator' 'use text format full_html' ";
-        $command = "drush --root=$root --uri=$subdomain.$domain/ $operation -y";
-        echo "COMMAND: " . $command . "\r\n";
-        $output = shell_exec($command);
+                $operation = " role-add-perm 'Enterprise Administrator' 'use text format filtered_html_tables' ";
+                $command = "drush --root=$root --uri=$subdomain.$domain/ $operation -y";
+                echo "COMMAND: " . $command . "\r\n";
+                $output = shell_exec($command);
 
-        $operation = " role-remove-perm 'Enterprise Administrator' 'use text format full_html' ";
-        $command = "drush --root=$root --uri=$subdomain.$domain/ $operation -y";
-        echo "COMMAND: " . $command . "\r\n";
-        $output = shell_exec($command);
+                $operation = " role-remove-perm 'Site Administrator' 'use text format full_html' ";
+                $command = "drush --root=$root --uri=$subdomain.$domain/ $operation -y";
+                echo "COMMAND: " . $command . "\r\n";
+                $output = shell_exec($command);
 
-        $command = "drush --root=$root --uri=$subdomain.$domain/ cc all -y";
-        echo "COMMAND: " . $command . "\r\n";
-        $output = shell_exec($command);
+                $operation = " role-remove-perm 'Enterprise Administrator' 'use text format full_html' ";
+                $command = "drush --root=$root --uri=$subdomain.$domain/ $operation -y";
+                echo "COMMAND: " . $command . "\r\n";
+                $output = shell_exec($command);
 
+                $command = "drush --root=$root --uri=$subdomain.$domain/ cc all -y";
+                echo "COMMAND: " . $command . "\r\n";
+                $output = shell_exec($command);
+            } else {
+                $subdomain = trim(str_replace($tgtChars, "", $pieces[1])); // delete single quotes and semicolons
+                echo $subdomain;
+                echo "\r\n";
+            }
+        }
     }
+    fclose($file);
 }
-
-fclose($file);
-?>
 
 
